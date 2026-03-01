bd8f720d187da8c76d6b3c442626159c13e93d94
Tracy Zhou <tracyzhou@google.com>
2026-02-28 22:17:18 -0800

    Launcher3: Support blur in Launcher folders
    We use a combination of cross window blur and render effect of the pre open drag layer screenshot so that we can blend the blurred icons with the blurred wallpaper.
    
    Bug: 445467156
    Test: http://recall/-/cJXSFKMnb60bY3b9qyndqO/jzM1UnC8ULP9wDBUV9m8B
    Flag: com.android.launcher3.blur_on_more_surfaces
    Change-Id: I05cde2128015806bfc3a89677c945da615dab1a5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- src/com/android/launcher3/folder/FolderPagedView.java
+++ src/com/android/launcher3/folder/FolderPagedView.java
@@ -41,6 +41,7 @@ import com.android.launcher3.CellLayout;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.PagedView;
 import com.android.launcher3.R;
+import com.android.launcher3.graphics.PathWrapper;
 import com.android.launcher3.ShortcutAndWidgetContainer;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.apppairs.AppPairIcon;
@@ -97,7 +98,7 @@ public class FolderPagedView extends PagedView<PageIndicatorDots> implements Cli
 
     private Folder mFolder;
 
-    private Path mClipPath;
+    private PathWrapper mClipPath;
 
     // If the views are attached to the folder or not. A folder should be bound when its
     // animating or is open.
@@ -154,7 +155,7 @@ public class FolderPagedView extends PagedView<PageIndicatorDots> implements Cli
     protected void dispatchDraw(Canvas canvas) {
         if (mClipPath != null) {
             int count = canvas.save();
-            canvas.clipPath(mClipPath);
+            canvas.clipPath(mClipPath.getPath());
             mFocusIndicatorHelper.draw(canvas);
             super.dispatchDraw(canvas);
             canvas.restoreToCount(count);
@@ -713,7 +714,7 @@ public class FolderPagedView extends PagedView<PageIndicatorDots> implements Cli
     }
 
     @Override
-    public void setClipPath(Path clipPath) {
+    public void setClipPath(PathWrapper clipPath) {
         mClipPath = clipPath;
         invalidate();
     }
